f9258f8ae913224143ca8445d21e00b8ab5c7d75
Kelp710 <bz1786354@gmail.com>
2022-08-12 19:04:18 -0400

    connect python backend with React

---
+++ app/main.py
@@ -0,0 +1,65 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Depends, FastAPI
+from app.dependencies import get_query_token, get_token_header
+# from .internal import admin
+from app.routers import currency
+
+# app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()
+app.include_router(currency.router)
+# app.include_router(
+#     admin.router,
+#     prefix="/admin",
+#     tags=["admin"],
+#     dependencies=[Depends(get_token_header)],
+#     responses={418: {"description": "I'm a teapot"}},
+# )
+# Updating data to the web page, jsx file can access then
+
+origins = [
+    "http://10.10.1.10:3000",
+]
+
+hard_countries ={"About":[
+# {'country': 'Haiti', 'alpha2Code': 'HT', 'currency': 'HTG', 'currecy_rate': 1.064025, 'capital': 'Port-au-Prince', 'region': 'Caribbean', 'picture': 'https://images.unsplash.com/photo-1580740240113-a5c3a287fe7e?ixid=MnwzNDU3NDN8MHwxfHJhbmRvbXx8fHx8fHwxfHwxNjYwMTEyODY1&ixlib=rb-1.2.1', 'safe_level': {'score': 4.4, 'sources_active': 7, 'message': 'Haiti has a current risk level of 4.4 (out of 5). We advise: Please reconsider your need to travel to Haiti.', 'updated': '2022-08-09 08:03:13', 'source': 'https://www.travel-advisory.info/haiti'}},
+# {'country': 'Seychelles', 'alpha2Code': 'SC', 'currency': 'SCR', 'currecy_rate': 0.168857, 'capital': 'Victoria', 'region': 'Eastern Africa', 'picture': 'https://images.unsplash.com/photo-1593427264099-20e0c3e7fdb2?ixid=MnwzNDU3NDN8MHwxfHJhbmRvbXx8fHx8fHwxfHwxNjYwMTEyODY1&ixlib=rb-1.2.1', 'safe_level': {'score': 3, 'sources_active': 4, 'message': 'Seychelles has a current risk level of 3 (out of 5). We advise: Use some caution when travelling Seychelles.', 'updated': '2022-08-09 08:03:13', 'source': 'https://www.travel-advisory.info/seychelles'}},
+{'country': 'Turkey', 'alpha2Code': 'TR', 'currency': 'TRY', 'currecy_rate': 0.069946, 'capital': 'Ankara', 'region': 'Western Asia', 'picture': 'https://images.unsplash.com/photo-1535916707207-35f97e715e1c?ixid=MnwzNDU3NDN8MHwxfHJhbmRvbXx8fHx8fHwxfHwxNjYwMTEyODY2&ixlib=rb-1.2.1', 'safe_level': {'score': 3.5, 'sources_active': 8, 'message': 'Turkey has a current risk level of 3.5 (out of 5). We advise: Please reconsider your need to travel to Turkey.', 'updated': '2022-08-09 08:03:13', 'source': 'https://www.travel-advisory.info/turkey'}},
+{'country': 'Zambia', 'alpha2Code': 'ZM', 'currency': 'ZMW', 'currecy_rate': 0.184511, 'capital': 'Lusaka', 'region': 'Eastern Africa', 'picture': 'https://images.unsplash.com/photo-1639736921908-4fc1e56370ca?ixid=MnwzNDU3NDN8MHwxfHJhbmRvbXx8fHx8fHwxfHwxNjYwMTEyODY2&ixlib=rb-1.2.1', 'safe_level': {'score': 3, 'sources_active': 4, 'message': 'Zambia has a current risk level of 3 (out of 5). We advise: Use some caution when travelling Zambia.', 'updated': '2022-08-09 08:03:13', 'source': 'https://www.travel-advisory.info/zambia'},"paragraph": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Why": [
+      "Good food",
+      "Tempor incididunt",
+      "Lorem ipsum dolor",
+      "Incididunt ut labore"
+    ],
+    "Why2": [
+      "Aliquip ex ea commodo",
+      "Lorem ipsum dolor",
+      "Exercitation ullamco",
+      "Lorem ipsum dolor"
+    ]}
+]}
+# hard_json = json.dumps(hard_countries)
+# print(hard_json)
+c = currency.CurrencyFinder("JPY","2020-03-03","2020-09-06")
+countries = c.find_countries()
+countries_data = {"About":countries}
+# print(countries_data)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# c = CurrencyFinder()
+# coin = c.find_countries("JPY","2020-03-03","2020-09-06")
+# print(coin)
+
+
+
+@app.get("/")
+@staticmethod
+def Hello():
+    return countries_data
---
+++ app/routers/currency.py
@@ -0,0 +1,127 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+import os
+from datetime import date
+from fastapi import APIRouter, Depends, HTTPException
+from app.dependencies import get_token_header
+
+router = APIRouter(
+    prefix="/countries",
+    tags=["countries"],
+    dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
+
+app = FastAPI()
+
+origins = [
+    "http://10.10.1.10:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class CurrencyFinder:
+    def __init__(self, base, start_date, end_date) -> None:
+      self.base = base
+      self.start_date = start_date
+      self.end_date = end_date
+
+    def find_countries(self):
+      url = f"https://api.apilayer.com/exchangerates_data/fluctuation?start_date={self.start_date}&end_date={self.end_date}&base={self.base}"
+      countries_info = []
+      payload = {}
+      headers= {
+        "apikey": "PyPZOB4LbjtqDiYMG6RmhWBKG76X5rH6"
+      }
+      unsplash_key = "BIWkxve6hsoQNq7zoauikNAOXOH03SEKh1futEFtnRA"
+
+      response = requests.request("GET", url, headers=headers, data = payload)
+
+      status_code = response.status_code
+      result = response.json()
+      for n in result["rates"]:
+        country_info = {}
+        change_pct = result["rates"][n]["change_pct"]
+        current_price = result["rates"][n]["end_rate"]
+        if change_pct >= 20:
+
+
+          # Get basic country`s info
+          url = f"https://restcountries.com/v2/currency/{n}"
+          r = requests.get(url)
+          if r.status_code == 404:
+            pass
+          else:
+            country_data = r.json()
+            country = country_data[0]["name"]
+            country_info["country"]=country
+            country_info["alpha2Code"] = country_data[0]['alpha2Code']
+            country_info["currency"]=n
+            country_info["currecy_rate"] = current_price
+            country_info["capital"] = country_data[0]["capital"]
+            country_info["region"] = country_data[0]["subregion"]
+            
+            # Get picture url of the country
+            url = f"https://api.unsplash.com/photos/random/?client_id={unsplash_key}&query={country}&per_page=4&order_by=popular&orientation=landscape&count=1&content_filter=high"
+            r_2 = requests.get(url) 
+            pic_data = r_2.json()[0]["urls"]["raw"]
+            country_info["picture"]=pic_data
+
+            # get safe level by country code
+            country_code = country_data[0]['alpha2Code']
+            url_safe = f"https://www.travel-advisory.info/api?countrycode={country_code}"
+            r_3 = requests.get(url_safe)
+            safe_data = r_3.json()['data'][country_code]['advisory']
+
+            country_info["safe_level"]=safe_data
+
+            countries_info.append(country_info)
+        else:
+          pass
+        
+      return countries_info
+      
+      
+
+    
+    
+c = CurrencyFinder("JPY","2020-03-03","2020-09-06")
+countries = c.find_countries()
+countries_data = {"About":countries}
+print(countries_data)
+
+@router.get("/")
+@staticmethod
+async def read_countries():
+    return countries_data
+
+# @router.get("/{country_id}")
+# async def read_country(country_id: str):
+#   countries_data = []
+#   for country_info in countries_info:
+#     if country_id not in country_info:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     return {"name": countries_info["About"][country_id], "item_id": country_id}
+
+# @router.put(
+#     "/{country_id}",
+#     tags=["custom"],
+#     responses={403: {"description": "Operation forbidden"}},
+# )
+# async def update_item(country_id: str):
+#     if country_id != "country":
+#         raise HTTPException(
+#             status_code=403, detail="You can only update the item: country"
+#         )
+#     return {"country_id": country_id, "name": "The cheap country"}
+
+# # currency = CurrencyFinder("CAD", "2020-02-02", "2020-06-02")
